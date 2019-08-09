18c8c70ce2f65ba6356d896d630592dd02056f55
Mryan <40586009+Mryan2005@users.noreply.github.com>
2019-08-09 19:57:43 +0800

    fix docker can't login

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker login -u ${docker_username} -p ${docker_password}
-docker tag noderrobot/core:build-${TRAVIS_JOB_ID} noderrobot/core:build-${TRAVIS_JOB_ID}
-docker push noderrobot/core:build-${TRAVIS_JOB_ID}
-docker logout
